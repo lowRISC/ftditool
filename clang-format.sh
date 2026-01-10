cccdb8661ea36ea6c099d95d8f10229d5ec496c3
Douglas Reis <doreis@lowrisc.org>
2026-01-10 15:17:12 +0000

    Add formating script

--- clang-format.sh
+++ clang-format.sh
@@ -1,5 +1,8 @@
 #! /usr/bin/env bash
+set -e
+[[ "$1" == "check" ]] && operation="-n" || operation="-i"
 
-find  -name "*.cc" -print0  | xargs -0 clang-format -i
-find  -name "*.hh" -print0  | xargs -0 clang-format -i
-echo "done"
+find  -name "*.cc" -print0  | xargs -0 clang-format $operation
+find  -name "*.hh" -print0  | xargs -0 clang-format $operation
+
+echo "$1 done"
