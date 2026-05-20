3f173a9debaa805926c0235e36ab2fa0bef0a7f1
Douglas Reis <doreis@lowrisc.org>
2026-05-20 13:05:02 +0100

    [nix] Use local mirror to host ftdichip artefacts
    FTDI's severs are bocking automated donwloads
    
    Signed-off-by: Douglas Reis <doreis@lowrisc.org>

--- nix/ftd2xx.nix
+++ nix/ftd2xx.nix
@@ -10,7 +10,7 @@ stdenv.mkDerivation rec {
   pname = "ftd2xx";
   version = "1.4.34";
   src = fetchzip {
-    url = "https://ftdichip.com/wp-content/uploads/2025/11/libftd2xx-linux-x86_64-${version}.tgz";
+    url = "https://storage.googleapis.com/lowrisc-ci-cache/external/libftd2xx-linux-x86_64-${version}.tgz";
     hash = "sha256-O47VO8DpNJt2VTjvyEwA4sxLegUrwdazeIh3JHW2URI=";
   };
 
